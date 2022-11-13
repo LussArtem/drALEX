efc6b34c21714842696388f9f887621a1d938613
Artem L <vlossaa@gmail.com>
2022-11-14 00:55:39 +0700

    new ` header burger`

--- src/js/app.js
+++ src/js/app.js
@@ -27,10 +27,13 @@ import * as flsFunctions from "./files/functions.js";
 flsFunctions.isWebp();
 /* Добавление класса touch для HTML если браузер мобильный */
 // flsFunctions.addTouchClass();
+
 /* Добавление loaded для HTML после полной загрузки страницы */
 // flsFunctions.addLoadedClass();
+
 /* Модуль для работы с меню (Бургер) */
-// flsFunctions.menuInit();
+flsFunctions.menuInit();
+
 /* Учет плавающей панели на мобильных устройствах при 100vh */
 // flsFunctions.fullVHfix();
 
